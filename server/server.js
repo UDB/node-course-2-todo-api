be1144bfb38bbfa8fb18b758336517372ce97526
Udit <udit@example.com>
2018-10-11 19:42:12 +0530

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const { ObjectID } = require('mongodb');
 let { mongoose } = require('./db/mongoose');
 let { Todo } = require('./models/todo');
 let { User } = require('./models/user');
+let {authenticate}= require('./middleware/authenticate');
 
 
 let app = express();
@@ -112,6 +113,10 @@ app.post("/users",(req,res)=>{
 		.catch((e)=>{
 			res.status(400).send(e);
 		})
+});
+
+app.get('/users/me',authenticate,(req,res)=>{
+	res.send(req.user);
 });
 
 app.listen(port, () => {
