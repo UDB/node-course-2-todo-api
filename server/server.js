a297b2c7af597c38d0ab9081886550c6eeabd321
Udit <udit@example.com>
2018-10-11 14:50:34 +0530

    Setup separate tests and development envs

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
-const _=require('lodash');
+require("./config/config");
+
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
@@ -10,7 +12,7 @@ let { User } = require('./models/user');
 
 
 let app = express();
-const port = process.env.PORT||3000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post("/todos", (req, res) => {
@@ -45,7 +47,7 @@ app.get("/todos/:id", (req, res) => {
 			if (!todo) {
 				return res.status(404).send();
 			}
-			res.status(200).send({todo});
+			res.status(200).send({ todo });
 		})
 		.catch((e) => {
 			return res.status(400).send();
@@ -53,48 +55,48 @@ app.get("/todos/:id", (req, res) => {
 
 });
 
-app.delete('/todos/:id',(req,res)=>{
-	let id= req.params.id;
+app.delete('/todos/:id', (req, res) => {
+	let id = req.params.id;
 
-	if(!ObjectID.isValid(id)){
+	if (!ObjectID.isValid(id)) {
 		return res.status(404).send();
 	}
 
 	Todo.findByIdAndDelete(id)
-		.then((todo)=>{
-			if(!todo){
+		.then((todo) => {
+			if (!todo) {
 				return res.status(404).send();
 			}
-			res.status(200).send({todo});
+			res.status(200).send({ todo });
 		})
-		.catch((e)=>{
+		.catch((e) => {
 			return res.status(400).send();
 		});
 });
 
-app.patch('/todos/:id',(req,res)=>{
-	let id=req.params.id;
-	let body= _.pick(req.body,['text','completed']);
+app.patch('/todos/:id', (req, res) => {
+	let id = req.params.id;
+	let body = _.pick(req.body, ['text', 'completed']);
 
-	if(!ObjectID.isValid(id)){
+	if (!ObjectID.isValid(id)) {
 		return res.status(404).send();
 	}
 
-	if(_.isBoolean(body.completed) && body.completed){
-		body.completedAt= new Date().getTime();
-	}else{
-		body.completed=false;
-		body.completedAt=null;
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
 	}
 
-	Todo.findByIdAndUpdate(id,{$set:body},{new:true})
-		.then((todo)=>{
-			if(!todo){
+	Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+		.then((todo) => {
+			if (!todo) {
 				return res.status(400).send();
 			}
-			res.send({todo});
+			res.send({ todo });
 		})
-		.catch((e)=>{res.status(400).send();})
+		.catch((e) => { res.status(400).send(); })
 })
 
 app.listen(port, () => {
