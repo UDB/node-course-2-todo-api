41e45a4de473e58302fa652fd847d3e3f884321c
Udit <udit@example.com>
2018-10-11 12:08:20 +0530

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -52,6 +52,24 @@ app.get("/todos/:id", (req, res) => {
 
 });
 
+app.delete('/todos/:id',(req,res)=>{
+	let id= req.params.id;
+
+	if(!ObjectID.isValid(id)){
+		return res.status(404).send();
+	}
+
+	Todo.findByIdAndDelete(id)
+		.then((doc)=>{
+			if(!doc){
+				return res.status(404).send();
+			}
+			res.status(200).send({doc});
+		})
+		.catch((e)=>{
+			return res.status(400).send();
+		});
+});
 
 app.listen(port, () => {
 	console.log(`Started on port ${port}`);
